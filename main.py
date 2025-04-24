84648cef71252d54b782f31f520f009b06ea7f06
student06 <jakub.gola2@teb.edu.pl>
2025-04-24 15:48:29 +0200

    24.04.2025

---
+++ main.py
@@ -0,0 +1,24 @@
+from PyQt6.QtWidgets import QDialog
+
+from layout import Ui_Dialog
+
+
+class MyForm(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
+
+    def binary:
+
+    def octal:
+
+    def decimal:
+
+    def hexadecimal:
+
+
+if __name__ == '__main__':
+
+
